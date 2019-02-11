3136d3c04a7040d178c423cc6532905a84e940db
vubinhne <vubinhcht@gmail.com>
2019-02-12 09:51:16 +1000

    reconstruct files

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-import average_precision
-import ranking_loss
 import numpy as np
+import measures
 
 preds = np.array([[0.2, 0.3, 0.5], [0.1, 0.6, 0.3], [0.2, 0.2, 0.6], [0.9, 0.1, 0]])
 truth = np.array([[1, 0, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]])
 
-ave_prec = average_precision.average_precision(preds, truth)
+ave_prec = measures.average_precision(preds, truth)
 
 print(ave_prec)
 
-rankloss = ranking_loss.ranking_loss(preds, truth)
+rankloss = measures.ranking_loss(preds, truth)
 print(rankloss)
