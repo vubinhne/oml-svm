9efe41d1594bd8e01d5de77ac3ff48c8d10950a9
vubinhne <vubinhcht@gmail.com>
2019-02-12 09:42:35 +1000

    Measure: Ranking Loss

---
+++ ranking_loss.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def ranking_loss(outputs, test_target):
+    # Computing the average precision
+    # Outputs: the predicted outputs of the classifier, the output of the ith instance for the jth class
+    #          is stored in Outputs(i,j)
+    # test_target: the actual labels of the test instances, if the ith instance belong to the
+    #          jth class, test_target(i, j) = 1, otherwise test_target(i, j) = 0
+
+    # Implemented by Vu: convert to the right format:
+    outputs = np.transpose(outputs)
+    test_target[test_target == 0] = -1
+    test_target = np.transpose(test_target)
+    # Done
+
+    [num_class, num_instance] = outputs.shape
+    temp_outputs = []
+    temp_test_target = []
+    for i in range(num_instance):
+        temp = test_target[:, i]
+        if np.sum(temp) != num_class and np.sum(temp) != -num_class:
+            temp_outputs.append(outputs[:, i])
+            temp_test_target.append(temp)
+
+    outputs = np.transpose(np.array(temp_outputs))
+    test_target = np.transpose(np.array(temp_test_target))
+    [num_class, num_instance] = outputs.shape
+
+    label = [None] * num_instance
+    label = [[] if v is None else v for v in label]
+    not_label = [None] * num_instance
+    not_label = [[] if v is None else v for v in not_label]
+    label_size = np.zeros((num_instance,))
+    for i in range(num_instance):
+        temp = test_target[:, i]
+        label_size[i] = np.sum(temp == np.ones(num_class, ))
+        for j in range(num_class):
+            if temp[j] == 1:
+                label[i].append(j)
+            else:
+                not_label[i].append(j)
+
+    rankloss = 0
+    for i in range(num_instance):
+        temp = 0
+        for m in range(int(label_size[i])):
+            for n in range(int(num_class - label_size[i])):
+                if outputs[label[i][m], i] <= outputs[not_label[i][n], i]:
+                    temp = temp + 1
+        rankloss = rankloss + temp / (label_size[i] * (num_class - label_size[i]))
+
+    return rankloss / num_instance
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import average_precision
+import ranking_loss
 import numpy as np
 
 preds = np.array([[0.2, 0.3, 0.5], [0.1, 0.6, 0.3], [0.2, 0.2, 0.6], [0.9, 0.1, 0]])
 truth = np.array([[1, 0, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]])
 
-res = average_precision.average_precision(preds, truth)
+ave_prec = average_precision.average_precision(preds, truth)
 
-print(res)
+print(ave_prec)
+
+rankloss = ranking_loss.ranking_loss(preds, truth)
+print(rankloss)
