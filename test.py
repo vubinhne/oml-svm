085970bfa3506af8af721ad1da718070a852d804
Vu Luong <s2983821@student.ad.griffith.edu.au>
2019-02-13 10:37:50 +1000

    Add adwin detector

--- test.py
+++ test.py
@@ -1,25 +1,37 @@
 import numpy as np
 import measures
 
-probs = np.array([[0.2, 0.3, 0.5], [0.1, 0.6, 0.3], [0.2, 0.2, 0.6], [0.9, 0.1, 0]])
-preds = np.array([[1, 1, 0], [1, 0, 0], [0, 1, 1], [0, 0, 1]])
-truth = np.array([[1, 0, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]])
+# probs = np.array([[0.2, 0.3, 0.5], [0.1, 0.6, 0.3], [0.2, 0.2, 0.6], [0.9, 0.1, 0]])
+# preds = np.array([[1, 1, 0], [1, 0, 0], [0, 1, 1], [0, 0, 1]])
+# truth = np.array([[1, 0, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]])
+#
+# ave_prec = measures.average_precision(probs, truth)
+#
+# print(ave_prec)
+#
+# rankloss = measures.ranking_loss(probs, truth)
+# print(rankloss)
+#
+# ex_acc = measures.example_based_accuracy_instances(preds, truth)
+# print(ex_acc)
+#
+# ex_f1 = measures.example_based_f1_instances(preds, truth)
+# print(ex_f1)
+#
+# macro_f1 = measures.macro_f1(preds, truth)
+# print(macro_f1)
+#
+# micro_f1 = measures.micro_f1(preds, truth)
+# print(micro_f1)
 
-ave_prec = measures.average_precision(probs, truth)
+a = []
+a.append([1, 2])
+a.append([3, 4])
 
-print(ave_prec)
+for i in range(10):
+    print(a)
 
-rankloss = measures.ranking_loss(probs, truth)
-print(rankloss)
-
-ex_acc = measures.example_based_accuracy_instances(preds, truth)
-print(ex_acc)
-
-ex_f1 = measures.example_based_f1_instances(preds, truth)
-print(ex_f1)
-
-macro_f1 = measures.macro_f1(preds, truth)
-print(macro_f1)
-
-micro_f1 = measures.micro_f1(preds, truth)
-print(micro_f1)
+    if i >= 5:
+        a = []
+        a.append([9, 10])
+        a.append([10, 11])
